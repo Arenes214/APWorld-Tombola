7e4fe34c6d8389d648534c2e7f4b2be18aaee6f2
Arenes214 <165089609+Arenes214@users.noreply.github.com>
2026-02-14 13:15:08 +0100

    Connect regions, also fix index in names

--- regions.py
+++ regions.py
@@ -13,13 +13,20 @@ def create_and_connect_regions(world: APTombolaWorld) -> None:
 
 def create_all_regions(world: APTombolaWorld) -> None:
     regions = []
+
     start_region = Region("The Table", world.player, world.multiworld)
     regions.append(start_region)
 
     for i in range (6):
-        reg = Region(f"Card {i}", world.player, world.multiworld)
+        reg = Region(f"Card {i+1}", world.player, world.multiworld)
         regions.append(reg)
 
     world.multiworld.regions += regions
 
 
+def connect_regions(world. APTombolaWorld) -> None:
+    start_region = world.get_region("The Table")
+
+    for i in range (6):
+        card = world.get_region(f"Card {i+1}")
+        start_region.connect(card, f"Look at Card {i+1}")
