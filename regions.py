20b5b4f1dd4ae492bcc56dde8d67aca175a213b5
Arenes214 <165089609+Arenes214@users.noreply.github.com>
2026-02-14 13:08:04 +0100

    Regions create

--- regions.py
+++ regions.py
@@ -7,3 +7,15 @@ from BaseClasses import Entrance, Region
 if TYPE_CHECKING:
     from .world import APTombolaWorld
 
+def create_and_connect_regions(world: APTombolaWorld) -> None:
+    create_all_regions(world)
+    connect_regions(world)
+
+def create_all_regions(world: APTombolaWorld) -> None:
+    regions = []
+
+    for i in range (6):
+        reg = Region(f"Card {i}", world.player, world.multiworld)
+        regions.append(reg)
+
+    world.multiworld.regions += regions
