5baf176ce0cac3bd59a5ef7382f88061d36253e1
Arenes214 <165089609+Arenes214@users.noreply.github.com>
2026-02-14 21:21:59 +0100

    Moved card generation yet again

--- world.py
+++ world.py
@@ -45,3 +45,86 @@ class APTombolaWorld(World):
         #return self.options.as_dict(
         #    "hard_mode", "hammer", "extra_starting_chest", "confetti_explosiveness", "player_sprite"
         #)
+
+
+    # Card Generation
+    def generate_cards():
+        # Create and shuffle numbers
+        sheet_columns = {
+            0: list(range(1,10)),
+            1: list(range(10,20)),
+            2: list(range(20,30)),
+            3: list(range(30, 40)),
+            4: list(range(40, 50)),
+            5: list(range(50, 60)),
+            6: list(range(60, 70)),
+            7: list(range(70, 80)),
+            8: list(range(80, 91)), # Last columns includes 90
+        }
+
+        for i in sheet_columns:
+            world.random.shuffle(sheet_columns[i])
+
+        # Get the numbers for each card
+        all_card_numbers = []
+
+        # For each card get the first 9 numbers from each column to follow the "at least one per column" rule
+        for card in range(6):
+            card_numbers = []
+
+            for col in range(9):
+                n = sheet_columns[col].pop()
+                card_numbers.append((col, n))
+                # Column count is not needed here since it will be 1 for each column
+            all_card_numbers.append(card_numbers)
+
+        # Now we can take the remaining numbers to have 15 per Card
+
+        #BUG: There is a possibility that later loops won't have enough columns to take from and will infinite loop
+        #TODO FIX this
+        for card_numbers in all_card_numbers:
+            col_count = defaultdict(lambda:1) # Create the column count at 1 since there is guaranteed exact 1 per column
+
+            while (len(card_numbers) < 15):
+                col = world.random.randint(0,8)
+
+                if col_count[col] >=3: # Don't take if we already have 3 in column
+                    continue
+                if not sheet_columns[col]: # And seriously don't try to take from an empty column
+                    continue
+
+                n = sheet_columns[col].pop()
+                card_numbers.append((col, n))
+                col_count[col] += 1
+
+        # Now that i have all of the cards' numbers, create the cards
+        cards = []
+
+        # And populate them
+        for card_numbers in all_card_numbers:
+            card = [[0] * 9 for _ in range (3)]
+            row_count = [0,0,0] # Create the row count for the "5 per row"" rule
+
+            for (col, n) in card_numbers:
+                possible_rows = [r for r in range(3) if row_count[r] < 5]
+                row = world.random.choice(possible_rows)
+                card[row][col] = n
+                row_count[row] += 1
+
+            # After poplulating them, order the columns
+            for col in range (9):
+                col_sorted = [card[row][col] for row in range(3) if card[row][col] != 0]
+                col_sorted.sort()
+
+                i = 0 # index of col_sorted basically
+                for row in range(3):
+                    if card[row][col] != 0:
+                        card[row][col] = col_sorted[i]
+                        i += 1
+            # After doing everything add the card to cards
+            cards.append(card)
+
+        return cards
+
+    cards = generate_cards()
+
