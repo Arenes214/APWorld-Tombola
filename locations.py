cd2665f2b426825bf76a094bb3ac7ca4ef54552a
Arenes214 <165089609+Arenes214@users.noreply.github.com>
2026-04-26 18:05:37 +0200

    Create Milestones and add to complete list

--- locations.py
+++ locations.py
@@ -19,7 +19,7 @@ DECINA_REWARD_COUNT_MAX = 3
 TOMBOLA_REWARD_COUNT_MAX = 4
 
 # Location id - Cards will be 5 digit (Format CSYDN)
-# C indicates Card (e.g. 3xxxx is card 3)
+# C indicates Card (e.g. 3xxxx is card 3) ("Card 7" are milestones)
 # S indicates which score it is (2 Ambo, 3 Terno, ..., 6 Decina, 7 Tombola) or special properties:
     # 8 indicates Card Unlock (Cardsanity)
 # Y indicates the kinda of Sanity it is (0 No Sanity, 1 Rowsanity)
@@ -34,6 +34,7 @@ def all_locations_to_id():
     complete_list.update(create_all_regular_card_score_locations())
     complete_list.update(create_all_cardsanity_unlock_locations())
     complete_list.update(create_all_rowsanity_score_locations())
+    complete_list.update(create_all_milestone_score_locations())
 
     return complete_list
 
@@ -94,6 +95,52 @@ def create_all_rowsanity_score_locations():
     return the_list
 
 
+# For Milestones, Locations id are as following (Format MTABC)
+# M Indicates:
+# 7 is Milestones in general
+# 8 is Total Count Milestones, since they may need more than 999 index
+
+# T indicates Type: 1 is Score Collect, 2 is Collection of Numbers,, 3 is Even/Odd
+
+# For Score Collect
+# A indicates which score it is (2 Ambo, 3 Terno...)
+# B indicates the number of them to get
+# C is 1
+
+# For Collection of Numbers
+# TODO A list will exists that contains all possible Collections
+# The format will just be based on the list
+
+# For Total Count, TABC will indicate the count to reach
+
+# For Even/Odd, A indicates Even(2) or Odd(1), BC indicates the count
+
+def create_all_milestone_score_locations():
+    the_list = {}
+    # Create all Score Collect locations
+    for score_id, score in enumerate(["Ambo","Terno","Quaterna","Cinquina","Decina","Tombola"]):
+        for count_id, count in enumerate(["Double","Triple","Quadruple","Quintuple","All of the"]):
+            the_list[f"Milestone - {count} {score}"] = 70000 + 1000 + (score_id+2)*100 + (count_id+1)*10 + 1
+
+    # Create all Collection of Numbers
+    # TODO for now only Developer Permutations exists
+    the_list["Milestone - Developer Permutations"] = 70000 + 2000 + 1
+    the_list["Milestone - Nice."] = 70000 + 2000 + 2
+    the_list["Milestone -  The Meaning of Life"] = 70000 + 2000 + 3
+
+    # Create all Total Count locations
+    # TODO for now only a select hardcoded amount of Total Counts exists
+    the_list["Milestone - Developer Count"] = 80000 + 214
+    the_list["Milestone - Developer Count x10"] = 80000 + 2140
+    the_list["Milestone - Erbs told me this number"] = 80000 + 690
+
+
+    # Create all Even/Odd locations
+    for i in range (20): # 20 is arbitrary, may be increased in future
+        the_list[f"Milestone - {i} Even Numbers"] = 70000 + 3000 + 200 + i
+        the_list[f"Milestone - {i} Odd Numbers"] = 70000 + 3000 + 100 + i
+
+
 # Create the list with ids to mimic APQuest behaviour
 LOCATION_NAME_TO_ID = all_locations_to_id()
 
@@ -110,7 +157,7 @@ def create_all_locations(world: APTombolaWorld) -> None:
     if world.options.rowsanity:
         create_rowsanity_locations(world)
 
-def create_regular_locations(world: APTombolaWorld) -> None:
+def create_regular_locations(world: APTombolaWorld) -> None: # ALSO CREATES CARDSANITY LOCKS
     # Get the regions
     # Card 1 is index **1** since index 0 will be the starting regions
     regions = []
@@ -134,7 +181,6 @@ def create_regular_locations(world: APTombolaWorld) -> None:
             loc = get_location_names_with_ids([f"Card {card} Unlocked"])
             regions[0].add_locations(loc, APTombolaLocation)
 
-
 def create_rowsanity_locations(world: APTombolaWorld) -> None:
     # Yes the first part is duplicated code from the regular locations
     # Get the regions
@@ -151,6 +197,12 @@ def create_rowsanity_locations(world: APTombolaWorld) -> None:
         region_index = value // 10000
         loc = get_location_names_with_ids([key])
         regions[region_index].add_locations(loc, APTombolaLocation)
+
+def create_milestone_locations(world: APTombolaWorld) -> None:
+
+
+
+
 
 
 
