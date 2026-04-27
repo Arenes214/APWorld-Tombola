f34ae454c69bcbff44f065eb864229b44705e92d
Arenes214 <165089609+Arenes214@users.noreply.github.com>
2026-04-27 21:26:53 +0200

    change words in yaml

--- options.py
+++ options.py
@@ -29,7 +29,7 @@ class PreventOtherMetaGameItems(DefaultOnToggle):
     """
     When enabled, prevents locations in the game from having items from other Meta-Games, such as APBingo.
 
-    Note: progression items from AP Tombola games will never be placed in an AP Tombola slot, even if this option is disabled.
+    Note: Progression items from AP Tombola slots will never be placed in an AP Tombola slot, even if this option is disabled.
     (Default: Enabled)
     """
     display_name = "Prevent Other Meta-Game Items"
@@ -59,7 +59,7 @@ class Rowsanity(Toggle):
     """
     If Rowsanity is enabled, for each row of a Card and for each score type other than Tombola,
     a location will be created that will require that specific row's numbers to be checked.
-    In the case of a Decina, each combination of 2 rows will have its check
+    In the case of a Decina, each combination of 2 rows will be a location.
     (Default: Disabled)
     """
     display_name = "Rowsanity"
